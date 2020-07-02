0710bf9bd578d833d88362381a670e73c88b4b6e
arielBWong <ariel.bingwang@outlook.com>
2020-07-02 14:36:17 +1000

    smd

--- problems/SMD/smd1.m
+++ problems/SMD/smd1.m
@@ -1,4 +1,4 @@
-classdef smd9
+classdef smd1
     properties
         p = 1;
         q = 2;
@@ -12,31 +12,32 @@ classdef smd9
         name;
     end
     methods
-        function obj = smd9(p, q, r)
+        function obj = smd1(p, q, r)
             if nargin == 3
                 obj.p = p;
                 obj.q = q;
                 obj.r = r;
             end
-            obj.name = 'SMD9';
-
+            obj.name = 'SMD1';
+            
             % level variables
             obj.n_lvar = obj.q + obj.r;
             obj.n_uvar = obj.p + obj.r;
             
+            % bounds
             %init bound upper level
             xu_bl_1 = ones(1, obj.p)* (-5.0);
             xu_bu_1 = ones(1, obj.p) * 10.0;
             xu_bl_2 = ones(1, obj.r)* (-5.0);
-            xu_bu_2 = ones(1, obj.r) * 1.0;
+            xu_bu_2 = ones(1, obj.r) * 10.0;
             obj.xu_bl = [xu_bl_1, xu_bl_2];
             obj.xu_bu = [xu_bu_1, xu_bu_2];
             
             % init bound lower level
             xl_bl_1 = ones(1, obj.q) * (-5.0);
             xl_bu_1 = ones(1, obj.q) * 10.0;
-            xl_bl_2 = ones(1, obj.r) * (-1.0 + 1e-10);
-            xl_bu_2 = ones(1, obj.r) * (-1.0 + exp(1));
+            xl_bl_2 = ones(1, obj.r) * (-pi/2 + 1e-10);
+            xl_bu_2 = ones(1, obj.r) * (pi/2 - 1e-10);
             obj.xl_bl = [xl_bl_1, xl_bl_2];
             obj.xl_bu = [xl_bu_1, xl_bu_2];
             
@@ -44,45 +45,30 @@ classdef smd9
         
         function [f, c] = evaluate_u(obj, xu, xl)
             xu1 = xu(:, 1 : obj.p);
-            xu2 = xu(:, obj.p+1 : obj.p+obj.r);
+            xu2 = xu(:, obj.p + 1: obj.p + obj.r);
             
             xl1 = xl(:, 1 : obj.q);
             xl2 = xl(:, obj.q+1 : obj.q+obj.r);
-            
-            f = sum((xu1).^2, 2) ...
-                - sum((xl1).^2, 2) ...
-                + sum((xu2).^2, 2) - sum((xu2 - log(1+xl2)).^2, 2);
-            
-           
-            
-            c(:, 1) = sum(xu1.^2, 2) ...
-                    + sum(xu2.^2, 2)...
-                    - floor(sum(xu1.^2, 2)+sum(xu2.^2, 2)+0.5);
-            
-            c = - c;
-            
-        end
-        
-        function[f, c] = evaluate_l(obj, xu, xl)
-            xu1 = xu(:, 1:obj.p);
-            xu2 = xu(:, obj.p+1:obj.p+obj.r);
-         
-            xl1 = xl(:, 1:obj.q);
-            xl2 = xl(:, obj.q+1:obj.q+obj.r);
-            
+            %-obj
             f = sum((xu1).^2, 2) ...
                 + sum((xl1).^2, 2) ...
-                + sum((xu2 - log(1+xl2)).^2, 2);
+                + sum((xu2).^2, 2) + sum((xu2 - tan(xl2)).^2, 2);
+            %-cie
+            c=[];
             
-           
+        end
+        function [f, c] = evaluate_l(obj, xu, xl)
+            xu1 = xu(:, 1 : obj.p);
+            xu2 = xu(:, obj.p + 1: obj.p + obj.r);
             
-            
-            %Write the constraints here
-            c = sum(xl1.^2, 2) ...
-                + sum(xl2.^2, 2) ...
-                - floor(sum(xl1.^2, 2) + sum(xl2.^2, 2)+0.5);
-            
-            c = - c;
+            xl1 = xl(:, 1 : obj.q);
+            xl2 = xl(:, obj.q+1 : obj.q+obj.r);
+            %-obj
+            f = sum((xu1).^2, 2) ...
+                + sum((xl1).^2, 2) ...
+                + sum((xu2 - tan(xl2)).^2, 2);
+            %-cie
+            c=[]
             
         end
     end
--- problems/SMD/smd7.m
+++ problems/SMD/smd7.m
@@ -1,4 +1,4 @@
-classdef smd9
+classdef smd7
     properties
         p = 1;
         q = 2;
@@ -12,18 +12,19 @@ classdef smd9
         name;
     end
     methods
-        function obj = smd9(p, q, r)
+        function obj = smd7(p, q, r)
             if nargin == 3
                 obj.p = p;
                 obj.q = q;
                 obj.r = r;
             end
-            obj.name = 'SMD9';
-
+            obj.name = 'SMD7';
+            
             % level variables
             obj.n_lvar = obj.q + obj.r;
             obj.n_uvar = obj.p + obj.r;
             
+            % bounds
             %init bound upper level
             xu_bl_1 = ones(1, obj.p)* (-5.0);
             xu_bu_1 = ones(1, obj.p) * 10.0;
@@ -35,8 +36,8 @@ classdef smd9
             % init bound lower level
             xl_bl_1 = ones(1, obj.q) * (-5.0);
             xl_bu_1 = ones(1, obj.q) * 10.0;
-            xl_bl_2 = ones(1, obj.r) * (-1.0 + 1e-10);
-            xl_bu_2 = ones(1, obj.r) * (-1.0 + exp(1));
+            xl_bl_2 = ones(1, obj.r) * (0 + 1e-10);
+            xl_bu_2 = ones(1, obj.r) * (exp(1));
             obj.xl_bl = [xl_bl_1, xl_bl_2];
             obj.xl_bu = [xl_bu_1, xl_bu_2];
             
@@ -44,45 +45,36 @@ classdef smd9
         
         function [f, c] = evaluate_u(obj, xu, xl)
             xu1 = xu(:, 1 : obj.p);
-            xu2 = xu(:, obj.p+1 : obj.p+obj.r);
+            xu2 = xu(:, obj.p + 1: obj.p + obj.r);
             
             xl1 = xl(:, 1 : obj.q);
             xl2 = xl(:, obj.q+1 : obj.q+obj.r);
             
-            f = sum((xu1).^2, 2) ...
+            %-obj
+            m = [1:obj.p];
+            f = 1+1/400*sum((xu1).^2, 2) - prod(cos(xu1./sqrt(m)), 2) ...
                 - sum((xl1).^2, 2) ...
-                + sum((xu2).^2, 2) - sum((xu2 - log(1+xl2)).^2, 2);
+                + sum((xu2).^2, 2) - sum((xu2 - log(xl2)).^2, 2);    
             
-           
-            
-            c(:, 1) = sum(xu1.^2, 2) ...
-                    + sum(xu2.^2, 2)...
-                    - floor(sum(xu1.^2, 2)+sum(xu2.^2, 2)+0.5);
-            
-            c = - c;
-            
+            %-cie
+            c = [];
         end
         
-        function[f, c] = evaluate_l(obj, xu, xl)
-            xu1 = xu(:, 1:obj.p);
-            xu2 = xu(:, obj.p+1:obj.p+obj.r);
-         
-            xl1 = xl(:, 1:obj.q);
-            xl2 = xl(:, obj.q+1:obj.q+obj.r);
+        function [f, c] = evaluate_l(obj, xu, xl)
+            xu1 = xu(:, 1 : obj.p);
+            xu2 = xu(:, obj.p + 1: obj.p + obj.r);
             
-            f = sum((xu1).^2, 2) ...
-                + sum((xl1).^2, 2) ...
-                + sum((xu2 - log(1+xl2)).^2, 2);
+            xl1 = xl(:, 1 : obj.q);
+            xl2 = xl(:, obj.q+1 : obj.q+obj.r);
             
-           
-            
-            
-            %Write the constraints here
-            c = sum(xl1.^2, 2) ...
-                + sum(xl2.^2, 2) ...
-                - floor(sum(xl1.^2, 2) + sum(xl2.^2, 2)+0.5);
-            
-            c = - c;
+            %-obj
+            f = sum((xu1).^3, 2) ...
+                    + sum((xl1).^2, 2) ...
+                    + sum((xu2 - log(xl2)).^2, 2);
+            %-cie
+            c = [];
+
+   
             
         end
     end
