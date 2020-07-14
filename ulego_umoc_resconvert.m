412f3f2d6cfc5f8d617707325659abef295030bb
arielBWong <ariel.bingwang@outlook.com>
2020-07-14 19:31:36 +1000

    update umoc resconvert

--- ulego_umoc_resconvert.m
+++ ulego_umoc_resconvert.m
@@ -1,31 +1,42 @@
-%% this method process experiment results 
+%% this method process experiment results
 %
 clearvars;
 close all;
 
- 
-seed = 1;
-% read a seed and 
+
+seedmax = 1;
+% read a seed and
 problems = { 'mobp5()', 'mobp7()','mobp8()','mobp9(6)','mobp10()','mobp11(6)' };
 methods = {'Ehv_eval', 'EIM_eval'};
+problem_folder = strcat(pwd,'\problems\MOBP');
+addpath(problem_folder);
 
-ii = 1;
-jj = 1;
 
-prob = eval(problems{ii});
-method = methods{jj};
+np = length(problems);
+nm = length(methods);
 
-savepath = strcat(pwd, '\result_folder\', prob.name, '_', method);
-savename_fu = strcat(savepath, '\fu_', num2str(seed),'.csv');
+for seed = 1: seedmax
+    for ii = 1: np
+        for jj = 1:nm
+            prob = eval(problems{ii});
+            method = methods{jj};
+            
+            savepath = strcat(pwd, '\result_folder\', prob.name, '_', method);
+            savename_fu = strcat(savepath, '\fu_', num2str(seed),'.csv');
+            
+            nd_front = csvread(savename_fu);
+            fig1 = gcf;
+            scatter(nd_front(:,1), nd_front(:,2),'ro', 'filled'); drawnow;
+            t = [prob.name,' ', method, ' seed ', num2str(seed) ];
+            title(t);
+            savename = strcat(savepath, '\nd_', num2str(seed),'.fig');
+            savefig(savename);
+            savename = strcat(savepath, '\nd_', num2str(seed),'.png');
+            
+            saveas(fig1, savename);
+            close(fig1);
+        end
+    end
+end
 
-nd_front = csvread(savename_fu);
-fig1 = gcf;
-scatter(nd_front(:,1), nd_front(:,2),'ro', 'filled'); drawnow;
-t = [prob.name,' ', method, ' seed ', num2str(seed) ];
-title(t);
-savename = strcat(savepath, '\nd_', num2str(seed),'.fig');
-savefig(savename);
-savename = strcat(savepath, '\nd_', num2str(seed),'.png');
-
-saveas(fig1, savename);
-close(fig1);
+rmpath(problem_folder);
