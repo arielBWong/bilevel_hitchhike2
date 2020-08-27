5b4f02d546b2cd060afbd71b2940fb0b32ff754a
arielBWong <ariel.bingwang@outlook.com>
2020-08-27 15:14:30 +1000

    new problems

--- problems/TP/tp2.m
+++ problems/TP/tp2.m
@@ -13,8 +13,7 @@ classdef tp2
         lopt = NaN; % double check needed
     end
     methods
-        function obj = mobp11(k)
-            obj.p = k;
+        function obj = tp2(k)
             obj.q = k;
             % level variables
             obj.n_lvar = obj.q;
@@ -23,18 +22,19 @@ classdef tp2
             % bounds
             %init bound upper level
             obj.xu_bl = ones(1, obj.p) * -1;
-            obj.xu_bu = ones(1, obj.p) * 1 ;
+            obj.xu_bu = ones(1, obj.p) * 2;
            
            
             % init bound lower level
             obj.xl_bl = ones(1, obj.q) * -1;
-            obj.xl_bu = ones(1, obj.q) * 1;      
+            obj.xl_bu = ones(1, obj.q) * 2;      
         end
         
         function [f, c] = evaluate_u(obj, xu, xl) 
             %-obj
-           f(:, 1) = sum(exp(-xu ./ (1+ abs(xl))), 2) + sum(sin(xu ./ (1+abs(xl))), 2);
-           f(:, 2) = sum(exp(-xl ./ (1 + abs(xu))), 2) + sum(sin(xl ./ (1+ abs(xu))), 2);
+           f(:, 1) = (xl(:, 1) - 1) .^ 2 + sum(xl(:, 2: obj.q) .^ 2, 2) + xu.^2;
+           f(:, 2) = (xl(:, 1) - 1) .^ 2 +  sum(xl(:, 2: obj.q) .^ 2, 2) + (xu - 1).^2;
+           f = sum(f, 2);
                  
             %-cie
             c= [];            
@@ -43,10 +43,12 @@ classdef tp2
         
         function [f, c] = evaluate_l(obj, xu, xl)          
             %-obj
-            f = sum( cos(abs(xu) .* xl) ,2) + sum(sin(xu - xl), 2);
+            f(:, 1) =  xl(:, 1) .^ 2 + sum(xl(:, 2: obj.q) .^ 2, 2) ;
+            f(:, 2) =  (xl(:, 1) - xu) .^ 2 + sum(xl(:, 2: obj.q) .^ 2, 2);
+            f = sum(f, 2);
             
             %-cie
-           c=xu + xl - 1;
+           c=[];
         
          end
     end
