582c306feb5b50a9e8ae90bff97ffb0618a6551e
arielBWong <ariel.bingwang@outlook.com>
2020-09-23 11:39:37 +1000

    new

--- ulego_resconvert.m
+++ ulego_resconvert.m
@@ -5,17 +5,15 @@ close all;
 %problem_folder = strcat(pwd,'\problems\SMD');
 %addpath(problem_folder);
 
-%problems = { 'smd9()', 'smd10()', 'smd11()','smd12()'};
+problems = { 'smd9()', 'smd10()', 'smd11()','smd12()'};
 
-problems = { 'smd1()','smd2()','smd3()','smd4()','smd5()','smd6()','smd7()',...
-    'smd8()','smd9()', 'smd10()','smd11()','smd12()',...
-    'bltp1()','bltp2()','bltp3()', 'bltp5()','bltp6()','bltp7()',...
-    'bltp8()','bltp9()', 'bltp10()','bltp11()'};
+% problems = { 'smd1()','smd2()','smd3()','smd4()','smd5()','smd6()','smd7()',...
+ %    'smd8()','smd9()', 'smd10()','smd11()','smd12()'};
 
 
 s = 11;
 seeds = linspace(1, s, s);
-algs = {'eim', 'gen', 'bel'};
+algs = {'eim', 'bel', 'gen'};
 
 np = length(problems);
 ns = length(seeds);
@@ -36,12 +34,12 @@ for i = 1:np
         filename = strcat(savepath, '_',  algs{k});
         for j = 1:ns
             seed = seeds(j);
-            singlerun_file = strcat(savepath, '\out_', num2str(seed),'.csv');
+            singlerun_file = strcat(filename, '\out_', num2str(seed),'.csv')
             smatrix = csvread(singlerun_file);                           % performance records
-            accuracyupper(j, (i-1)*np + k) = smatrix(1,1);     % upper accuracy
-            accuracylower(j, (i-1)*np + k) = smatrix(1,2);     % lower accuracy
-            feasiupper(j, (i-1)*np + k)    = smatrix(3, 1);        % upper feasi
-            feasilower(j, (i-1)*np + k)   = smatrix(3, 2);             % lower feasi
+            accuracyupper(j, (i-1)*na + k) = smatrix(1,1);     % upper accuracy
+            accuracylower(j, (i-1)*na + k) = smatrix(1,2);     % lower accuracy
+            feasiupper(j, (i-1)*na + k)    = smatrix(3, 1);        % upper feasi
+            feasilower(j, (i-1)*na + k)   = smatrix(3, 2);             % lower feasi
         end
     end
 end
@@ -49,9 +47,8 @@ end
 
 output_path = strcat(pwd, '\result_folder\smd_convertmean.csv');
 fp=fopen(output_path,'w');
-fprintf(fp, 'seed,');
+fprintf(fp, 'problem,');
 for i = 1:na
-    fprintf(fp, '%s,', 'problem');
     method = algs{i};
     header = strcat(method,'_ul');
     fprintf(fp, '%s,', header);
@@ -66,18 +63,18 @@ for i = 1:np
     fprintf(fp, '%s, ',  prob.name);
     
     for jj = 1: na
-        index = (i-1) * np + jj;
+        index = (i-1) * na + jj;
         
         % calculate upper level mean accuracy
-        mean =  mean( accuracyupper(:, index ));
-        std = std( accuracyupper(:, index ));
-        item = strcat(num2str(mean), char(177), num2str(std));
+        mea =  mean( accuracyupper(:, index ));
+        st= std( accuracyupper(:, index ));
+        item = strcat(num2str(mea), char(177), num2str(st));
         fprintf(fp, '%s,', item);
         
         % calculate lower level mean accuracy
-        mean =  mean( accuracylower(:, index ));
-        std = std( accuracylower(:, index ));
-        item = strcat(num2str(mean), char(177), num2str(std));
+        mea =  mean( accuracylower(:, index ));
+        st = std( accuracylower(:, index ));
+        item = strcat(num2str(mea), char(177), num2str(st));
         fprintf(fp, '%s,', item);
     end
     fprintf(fp, '\n');
@@ -87,9 +84,9 @@ fclose(fp);
 
 output_path = strcat(pwd, '\result_folder\smd_convertmedian.csv');
 fp=fopen(output_path,'w');
-fprintf(fp, 'seed,');
+fprintf(fp, 'problem,');
 for i = 1:na
-    fprintf(fp, '%s,', 'problem');
+
     
     method = algs{i};
     header = strcat(method,'_ul');
@@ -110,33 +107,38 @@ fprintf(fp, '\n');
 
 %
 for ii = 1: np
-    prob =  eval(problems{i});
+    prob =  eval(problems{ii});
     fprintf(fp, '%s, ',  prob.name);
     
-    middle = round(seedmax/2);
+    middle = round(s/2);
     for jj = 1:na
-        index =  (i-1) * np + jj;
+        index =  (ii-1) * na + jj;
         
         % calculate upper level median accuracy
         one_column = accuracyupper(:, index);
         [~, ids] = sort(one_column);
         med = one_column(ids(middle));
+        medseed = ids(middle);
         fprintf(fp, '%s,', num2str(med));
         
         % extract corresponding lower level accuracy
         one_column = accuracylower(:, index);
-        med = one_column(ids(middle));
+        med = one_column(medseed);
         fprintf(fp, '%s,', num2str(med));
         
         %  feasibility ul
         one_column = feasiupper(:, index);
-        feasi = one_column(ids(middle));
+        feasi = one_column(medseed);
         fprintf(fp, '%s,', num2str(feasi));
         
         %  feasibility ll
         one_column = feasilower(:, index);
-        feasi = one_column(ids(middle));
+        feasi = one_column(medseed);
         fprintf(fp, '%s,', num2str(feasi));
+        
+        % seed
+        
+        fprintf(fp, '%s,', num2str(medseed));
     end
     fprintf(fp, '\n');
 end
